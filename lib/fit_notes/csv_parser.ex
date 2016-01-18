e3f7d46c1916185ddf224a289f3c2e3618f239ea
George Opritescu <ssscripting@gmail.com>
2016-01-18 18:25:45 +0100

    WIP adding csv support

---
+++ lib/fit_notes/csv_parser.ex
@@ -0,0 +1,5 @@
+defmodule FitNotes.CsvParser do
+  def parse(input_file) do
+    
+  end
+end
---
+++ test/lib/fit_notes/csv_parser_test.exs
@@ -0,0 +1,7 @@
+defmodule FitNotes.CsvParserTest do
+  use ExUnit.Case
+
+  test "it should work" do
+    FitNotes.CsvParser.parse("")
+  end
+end
