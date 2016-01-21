1fe035855729624acafa99097f27fbd280857561
George Opritescu <ssscripting@gmail.com>
2016-01-21 20:15:43 +0100

    tests for adapted

--- lib/fit_notes/csv_to_object_mapper.ex
+++ lib/fit_notes/csv_to_object_mapper.ex
@@ -2,9 +2,11 @@ defmodule FitNotes.CsvToObjectMapper do
   alias StatsViewer.ExerciseEntry
 
   def adapt(csv_results) do
-    for result <- Enum.drop(csv_results,1) do
+    for result <- csv_results do
       %ExerciseEntry{
-        
+        date: result.date, exercise: result.exercise,
+        category: result.category, weight: result.weight,
+        reps: result.reps
       }
     end
   end
--- test/lib/fit_notes/csv_to_object_mapper_test.exs
+++ test/lib/fit_notes/csv_to_object_mapper_test.exs
@@ -4,9 +4,24 @@ defmodule FitNotes.CsvToObjectMapperTest do
 
   require IEx
 
-  test "it should work to parse csv results" do
-    adapted = FitNotes.CsvParser.parse(Path.join(__DIR__, "fit_notes_export.csv")) |>
+  test "it should work to map to db objects" do
+    entries = FitNotes.CsvParser.parse_into_entries(Path.join(__DIR__, "fit_notes_export.csv")) |>
       CsvToObjectMapper.adapt
-    assert Enum.count(adapted) == 2
+    assert Enum.count(entries) == 2
+
+    first = Enum.at(entries, 0)
+    assert {2015, 9, 17} == {first.date.year, first.date.month, first.date.day}
+    assert "Behind The Neck Barbell Press" == first.exercise
+    assert "Shoulders" == first.category
+    assert 30.0 == first.weight
+    assert 10 == first.reps
+
+    second = Enum.at(entries, 1)
+
+    assert {2015, 9, 21} == {second.date.year, second.date.month, second.date.day}
+    assert "Stiff-Legged Deadlift" == second.exercise
+    assert "Legs" == second.category
+    assert 30.0 == second.weight
+    assert 10 == second.reps
   end
 end
