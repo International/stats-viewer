71b1ea437674b31f85908aaaaadf4a4d0a9baf3f
George Opritescu <ssscripting@gmail.com>
2016-01-21 18:25:14 +0100

    first test case

--- test/lib/fit_notes/csv_parser_test.exs
+++ test/lib/fit_notes/csv_parser_test.exs
@@ -1,8 +1,10 @@
 defmodule FitNotes.CsvParserTest do
   use ExUnit.Case
 
+  require IEx
+
   test "it should work" do
     result = FitNotes.CsvParser.parse(Path.join(__DIR__, "fit_notes_export.csv"))
-    assert result
+    assert Enum.count(result) == 21
   end
 end
