9e29f80df26927aa4961f9b638a7f8ea74ec8f0d
George Opritescu <ssscripting@gmail.com>
2016-01-28 19:15:37 +0100

    basic usage of react-router

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -23,13 +23,37 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Router, Route, Link } from 'react-router'
 
-let HelloWorld = React.createClass({
+const App = React.createClass({
   render() {
-    return (<h1>Hello React</h1>)
+    return (
+      <div>
+        <h1>App</h1>
+
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/inbox">Inbox</Link></li>
+        </ul>
+
+        {this.props.children}
+      </div>
+    )
   }
 })
 
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('root')
-);
+const About = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the about page</h1>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App}>
+      <Route path="about" component={About} />
+    </Route>
+  </Router>
+), document.getElementById('root'))
